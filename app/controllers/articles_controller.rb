80fc7195eeec70872e7020b21c24b5cee688a811
Andrea Tredrea <andrea.tredrea@gmail.com>
2021-05-10 22:05:55 +0200

    Code refactoring and partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
- 
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
-    @article = Article.find(params[:id])
+    
   end
 
 
@@ -15,9 +15,13 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+   
+  end
+
   
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created"
       redirect_to article_path(@article)
@@ -26,18 +30,29 @@ class ArticlesController < ApplicationController
     end
   end
 
-
-  
-  def edit
-  end
-
-  
   def update
-   
+   if @article.update(article_params)
+      flash[:notice] = "Article was updated"
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
+  
 
   def destroy
-    
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 
   
