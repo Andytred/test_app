0e6620d5f40ca881930baa8b425e9bbd7ed2f9d4
Andrea Tredrea <andrea.tredrea@gmail.com>
2021-05-14 11:27:03 +0200

    Adding admin, delete posibility for admin, and adding deleted paggination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,11 @@ class UsersController < ApplicationController
   before_action :require_same_user, only: [:edit, :update]
     
     def show
-        @articles = @user.articles
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
     def index
-        @users = User.all
+         @users = User.paginate(page: params[:page], per_page: 5)
     end
 
     def new
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
 
     def destroy
         @user.destroy
-        session[:user_id] = nil
+        session[:user_id] = nil if @user == current_user
         flash[:notice] = "Account and all associated articles successfully deleted"
         redirect_to articles_path
     end
@@ -60,7 +60,7 @@ private
     end
 
     def require_same_user
-        if current_user != @user
+        if current_user != @user && !current_user.admin?
             flash[:alert] = "You can only edit your own account"
             redirect_to @user
         end
