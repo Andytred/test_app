cf4893ae8c03b20aba8dc000e45304f43c226d11
Andrea Tredrea <andrea.tredrea@gmail.com>
2021-05-15 15:16:35 +0200

    Creating many through many model, controller Category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
     before_action :require_admin, except: [:index, :show]
     
+    
     def new
         @category = Category.new
     end
@@ -21,6 +22,7 @@ class CategoriesController < ApplicationController
 
     def show
         @category = Category.find(params[:id])
+        @articles = @category.articles.paginate(page: params[:page], per_page: 5)
     end
 
     private
@@ -30,9 +32,9 @@ class CategoriesController < ApplicationController
     end
 
     def require_admin
-      if !(logged_in? && current_user.admin?)
-        flash[:alert] = "Only admins can perform that action"
-        redirect_to categories_path
-      end
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
     end
+  end
 end
